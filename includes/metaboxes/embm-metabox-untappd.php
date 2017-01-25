7c8ac6c89a6bf97abcdc3439be947b4a1143d61d
Erin Morelli <erin@erinmorelli.com>
2017-01-24 16:32:05 -0800

    WIP - Updating metabox permissions and nonces

--- includes/metaboxes/embm-metabox-untappd.php
+++ includes/metaboxes/embm-metabox-untappd.php
@@ -256,7 +256,7 @@ function EMBM_Admin_Metabox_Untappd_save($post_id)
     }
 
     // Check user permissions
-    if (!current_user_can('edit_post')) {
+    if (!current_user_can('edit_post', $post_id)) {
         return;
     }
 
